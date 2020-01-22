2146bd532d359a389112cb075c05d5a5070f922b
Anonymous <lokeshkumar.rs@gmail.com>
2020-01-22 22:23:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "default" {
 
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
-  name = "terraform-jenkins-new-sg"
+  name = "sample-sg"
 
   ingress {
     from_port   = 80
@@ -36,5 +36,10 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
