9e53a2fc434bcea4d017848179526c82958ec016
Anonymous <lokeshkumar.rs@gmail.com>
2020-03-26 20:12:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+# Terraform state will be stored in S3
+terraform {
+  backend "s3" {
+    bucket = "terraform-bucket-alex"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+#Use Aws Terraform Provider
 provider "aws"{
 region = "${var.aws_region}"
 }
